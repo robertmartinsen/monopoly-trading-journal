487d49dba4b1f7fab9ce0dcd27f9a0a6c8a5d8f2
robertmartinsen <robertmartinsen2000@gmail.com>
2024-06-05 15:29:07 +0200

    cva

--- mtj/src/components/Button.tsx
+++ mtj/src/components/Button.tsx
@@ -1,7 +1,32 @@
-import { cva } from "class-variance-authority";
+import { VariantProps, cva } from "class-variance-authority";
 
-const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"]);
+const buttonStyles = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"]
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      idcon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default"
+  },
+})
+
+type ButtonProps = VariantProps<typeof buttonStyles>
 
 export function Button() {
-  return <button />;
+  return <button className={buttonStyles ({ variant, size})} />;
 }
